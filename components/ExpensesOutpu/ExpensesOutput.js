3b1aec0b7f704f8a3d6c26a563cfc5972aca719b
Evans-Boateng <eb5337877@gmail.com>
2025-01-15 08:17:48 +0000

    Dummy expenses data added to the expenses output file

--- components/ExpensesOutpu/ExpensesOutput.js
+++ components/ExpensesOutpu/ExpensesOutput.js
@@ -2,6 +2,15 @@ import { FlatList } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    description: 'A pair of shoes',
+    amount: 59.99,
+    data: new Date('2025-01-15')
+  }
+]
+
 function ExpensesOutput({expenses, expensesPeriod}) {
   return (
     <View>
