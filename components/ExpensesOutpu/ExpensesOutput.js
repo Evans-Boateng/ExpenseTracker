52712b45db4d64ccf42f02811f8bb970c08c80bb
Evans-Boateng <eb5337877@gmail.com>
2025-01-16 04:27:02 +0000

    We now have a perfect styling for the parent page

--- components/ExpensesOutpu/ExpensesOutput.js
+++ components/ExpensesOutpu/ExpensesOutput.js
@@ -1,6 +1,7 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
+import { GlobalStyles } from "../../constants/stylse";
 
 const DUMMY_EXPENSES = [
   {
@@ -37,10 +38,19 @@ const DUMMY_EXPENSES = [
 
 function ExpensesOutput({expenses, expensesPeriod}) {
   return (
-    <View>
+    <View style={styles.container}>
       <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
       <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700
+  }
+});
+
 export default ExpensesOutput;
