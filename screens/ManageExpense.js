f1f967b447a99dd2a8ad0695780383353393919b
Evans-Boateng <eb5337877@gmail.com>
2025-02-01 11:37:20 +0000

    Now the manage expense screen is a modal and the title changes whether user would add or edit an expense

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,6 +1,15 @@
 import { Text } from "react-native";
+import { useLayoutEffect } from "react";
+function ManageExpense({route, navigation}) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditting = !!editedExpenseId;
 
-function ManageExpense() {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditting ? 'Edit Expense' : 'Add Expense'
+    }); 
+  }, [navigation, isEditting]);
+
   return (
     <Text>ManageExpense screen</Text>
   )
