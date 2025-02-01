c0c15518184a767416660a85f1bbe2f3f140b411
Evans-Boateng <eb5337877@gmail.com>
2025-02-01 12:43:04 +0000

    Added a trash icon and background styling to manage expense

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,7 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { useLayoutEffect } from "react";
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/stylse";
 function ManageExpense({route, navigation}) {
   const editedExpenseId = route.params?.expenseId;
   const isEditting = !!editedExpenseId;
@@ -10,8 +12,37 @@ function ManageExpense({route, navigation}) {
     }); 
   }, [navigation, isEditting]);
 
+  function deleteExpenseHandler () {}
+
   return (
-    <Text>ManageExpense screen</Text>
+    <View style={styles.container}>
+      {isEditting && (
+        <View style={styles.deleteContainer}>
+          <IconButton 
+            icon='trash'
+            color={GlobalStyles.colors.error500} 
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
   )
 }
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center'
+
+  }
+})
